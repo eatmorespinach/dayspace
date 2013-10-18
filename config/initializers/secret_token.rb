693188689fa5a8dcac4d3e528359c8747f433c85
Drew Sing <sing.drew@gmail.com>
2013-10-18 09:52:26 -0700

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dayspace::Application.config.secret_key_base = 'bbb5f98a3982137fde37651d1d3b66483530e2c1f0edc5e539cd16936e5181b084c5c498beef53227d31904416daa3809df0802e8c6338f8630405a0067db5b4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dayspace::Application.config.session_store :cookie_store, key: '_dayspace_session'
