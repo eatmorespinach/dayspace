5fdfa6c03908af463252ca521f97258a481cb872
Drew Sing <sing.drew@gmail.com>
2013-11-01 18:08:51 -0700

    configured pg

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131101002707) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "listings", force: true do |t|
     t.integer  "work_spots"
     t.string   "city"
